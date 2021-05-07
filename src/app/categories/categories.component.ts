ee3832862e963f32fc08f1b23c62d47c6cd9fd14
YOUR NAME <YOUR@EMAIL.com>
2021-05-07 14:40:20 -0400

    generate and prepare categories component

---
+++ src/app/categories/categories.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Category } from '../category';
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+
+export class CategoriesComponent implements OnInit {
+  category: Category = {
+    id: 1,
+    title: "Javascript",
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
