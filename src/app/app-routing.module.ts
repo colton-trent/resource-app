46625911c197d8f54547af1e196324e94e3d85d8
YOUR NAME <YOUR@EMAIL.com>
2021-05-08 20:14:26 -0400

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
+import { ResourcesComponent } from './resources/resources.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'resource', component: ResourcesComponent},
+  {path: '', component: CategoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    ResourcesComponent
   ],
   imports: [
     BrowserModule,
